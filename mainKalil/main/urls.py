e05c9df62df677b85bf27f54006b4ffa050dab87
daniil_dn <dosindaniilm@gmail.com>
2022-12-02 15:39:49 +0300

    register view update

--- mainKalil/main/urls.py
+++ mainKalil/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import Register
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('adminData', views.adminData, name='adminData'),
+    path('register/', Register.as_view(), name='register')
 ]
--- mainKalil/main/views.py
+++ mainKalil/main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.views import View
+from django.shortcuts import render, redirect
+
+from .forms import UserCreationForm
 
 
 def index(request):
@@ -7,3 +12,28 @@ def index(request):
 
 def adminData(request):
     return render(request, 'main/adminData.html')
+
+
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        context = {
+            'form': UserCreationForm()
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        context = {
+            'form': form
+        }
+        return render(request, self.template_name, context)
